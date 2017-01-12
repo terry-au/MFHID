e29c33b25db02aa12b347f62855fdd82196ff5e0
terry1994 <terry1994dev@gmail.com>
2017-01-12 17:40:07 +0100

    Added foohid MIT copyright notice.

--- MFHID/Gamepad/HIDController.mm
+++ MFHID/Gamepad/HIDController.mm
@@ -3,6 +3,30 @@
 // Copyright (c) 2017 Terry Lewis. All rights reserved.
 //
 
+// Component adapted from the foohid mouse example
+//
+//The MIT License (MIT)
+//
+//Copyright (c) 2015 unbit
+//
+//        Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//        copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//        The above copyright notice and this permission notice shall be included in all
+//        copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 #import <cstdint>
 #import <iostream>
 #import <IOKit/IOKitLib.h>
@@ -382,8 +406,6 @@ bool HIDController::initialiseDriver() {
 void HIDController::sendHIDMessage() {
     // Arguments to be passed through the HID message.
 
-//    uint32_t field_count = 4;
-//    uint64_t fields[field_count];
     mSendMessage.fields[0] = (uint64_t) mInput[0];  // device name
     mSendMessage.fields[1] = strlen((char *) mInput[0]);  // name length
     mSendMessage.fields[2] = (uint64_t) &mReport;  // mouse struct
